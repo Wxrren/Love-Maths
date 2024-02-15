7b5ee550417404bf7ed23836dba1fe9d49bb0cf1
Wxrren <warren.lee.smith@hotmail.co.uk>
2024-02-15 19:54:37 +0000

    ADD: Set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,36 @@
-console.log("Connected!")
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function diplayAdditionQuestion() {
+
+}
+
+function displaySubtractionQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+function displayDivisionQuestion() {
+    
+}
+
