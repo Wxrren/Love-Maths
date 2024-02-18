b5cbe13057ef831593cb723c0cac5fea5dc2e51d
Wxrren <warren.lee.smith@hotmail.co.uk>
2024-02-18 13:42:23 +0000

    ADD: Subtraction game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,6 +31,11 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
+    } else if (gameType === "subtract") {
+        displaySubtractionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
     }
 } 
 
@@ -68,6 +73,8 @@ function calculateCorrectAnswer() {
         return[operand1 + operand2, "addition"];
     } else if (operator === "x") {
         return[operand1 * operand2, "multiply"];
+    } else if (operator === "-") {
+        return[operand1 - operand2, "subtract"];
     } else {
         alert(`Uninmplemted operator ${gameType}`);
         throw `Uninmplemted operator ${gameType}. Aborting`;
@@ -97,8 +104,8 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractionQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "-";
 }
 
