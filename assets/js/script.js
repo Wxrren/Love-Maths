8c06e4e1f07b54b38d84323a24cdb0fa58335181
Wxrren <warren.lee.smith@hotmail.co.uk>
2024-02-18 12:59:10 +0000

    ADD: Increment score and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,8 +46,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("You got it right! Great job!");
+        incrementScore();
     } else {
         alert(`Awwww...You answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -71,12 +73,20 @@ function calculateCorrectAnswer() {
     }
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 function incrementScore() {
-
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
