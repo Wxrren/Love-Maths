4911d527f1f2c5f57cc12034d147241f9354df2d
Wxrren <warren.lee.smith@hotmail.co.uk>
2024-02-18 15:04:23 +0000

    ADD: division game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,6 +43,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractionQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -85,6 +87,8 @@ function calculateCorrectAnswer() {
         return[operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return[operand1 - operand2, "subtract"];
+    } else if (operator === "÷") {
+        return[Math.floor(operand1 / operand2), "division"];
     } else {
         alert(`Uninmplemted operator ${gameType}`);
         throw `Uninmplemted operator ${gameType}. Aborting`;
@@ -126,8 +130,13 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivisionQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    let largeOperand = operand1 > operand2 ? operand1 : operand2;
+    let smallOperand = operand1 > operand2 ? operand2 : operand1;
+    let remainder = largeOperand % smallOperand;
+    let divisibleOperand = largeOperand - remainder;
+
+    document.getElementById('operand1').textContent = divisibleOperand;
+    document.getElementById('operand2').textContent = smallOperand;
     document.getElementById('operator').textContent = "÷";
 }
 
